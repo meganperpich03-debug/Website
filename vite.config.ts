11c02d7bf1393bfd1d752cf99e8eaf64188d9a5e
Megan <you@example.com>
2026-01-17 13:42:34 +0100

    fix path

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, '.', '');
-  const base = mode === 'production' ? '/Website/' : '/';
+  const base = '/';
   return {
     base,
     server: {
